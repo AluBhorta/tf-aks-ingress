7a1af695fa20a23503c80154d0ce35dcd88fcaf6
alu <dev.bhorta@gmail.com>
2023-04-14 17:01:13 +0600

    add documentation on readme

--- main.tf
+++ main.tf
@@ -24,16 +24,6 @@ provider "azurerm" {
   features {}
 }
 
-# provider "kubernetes" {
-#   config_path = "~/.kube/config"
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     config_path = "~/.kube/config"
-#   }
-# }
-
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
